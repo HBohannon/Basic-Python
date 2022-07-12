064792c266736150624f65b2843d48a2343ea0bd
Hanna B <hannabohannon@gmail.com>
2022-07-11 23:57:08 -0400

    Create Basic Python.py

---
+++ Basic Python.py
@@ -0,0 +1,65 @@
+# Create three variables that are set to the following questions:
+#What is your name?
+#How old are you?
+#How many months is that?
+
+myName = ("Hanna Bohannon")
+print(myName)
+
+myAge = 32
+print(myAge)
+
+myAge_in_months = (32*12)
+print(myAge_in_months)
+
+# Create three more variables that are set to the following answers to the above questions:
+#My name is "Billy".
+#30
+#360
+
+myName = ("Billy")
+print(myName)
+
+myAge = 30
+print(myAge)
+
+myAge_in_months = (30*12)
+print(myAge_in_months)
+
+#3.	Use the print function to print each of the variables. 
+
+question_1 = " What is your name? "
+print(question_1)
+
+answer_1 = 'My name is " Billy"'
+print(answer_1)
+
+question_2 = " How old are you?"
+print(question_2)
+
+answer_2 = 30
+print(answer_2)
+
+question_3 = " How many months is that?"
+print(question_3)
+
+answer_3 = (30*12)
+print(answer_3)
+
+# Comment out the appropriate variables and print functions from above.
+
+#What is your name?
+#My name is "Billy".
+#How old are you?
+#30
+
+# Create three new variables using these types of data my_string, my_integer, and my_float.
+
+my_thought = "Using Python is much easier"
+print(my_thought)
+
+multiply_two_numbers = 111*4
+print(multiply_two_numbers)
+
+add_two_float_numbers = 3 + 0.33
+print(add_two_float_numbers) 
